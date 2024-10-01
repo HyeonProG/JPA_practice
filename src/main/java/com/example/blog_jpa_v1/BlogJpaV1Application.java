d4fc0e1501b6f7edb779e4fdabaf46aa08ba1da1
HyeonProG <guswo3443@naver.com>
2024-10-01 10:17:08 +0900

    first commit

---
+++ src/main/java/com/example/blog_jpa_v1/BlogJpaV1Application.java
@@ -0,0 +1,13 @@
+package com.example.blog_jpa_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogJpaV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogJpaV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blog_jpa_v1/BlogJpaV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blog_jpa_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogJpaV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
