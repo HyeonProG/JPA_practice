fffd201c03f7a2df72f9f7c462d066792b4f94fc
HyeonProG <guswo3443@naver.com>
2024-10-02 17:15:22 +0900

    글 수정 API 만들기

--- src/main/java/com/example/demo/_domain/blog/controller/BlogApiController.java
+++ src/main/java/com/example/demo/_domain/blog/controller/BlogApiController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -56,6 +57,17 @@ public class BlogApiController {
         Article article = blogService.findById(id);
 
         return new ApiUtil<>(article);
+    }
+
+    // URL 즉, 주소 설계 - http://localhost:8080/api/article/1
+    @PutMapping("/api/article/{id}")
+    public ApiUtil<?> updateArticle(@PathVariable(name = "id") Integer id, @RequestBody ArticleDTO dto) {
+
+        // 1. 인증 검사
+        // 2. 유효성 검사
+        Article updateArticle = blogService.update(id, dto);
+
+        return new ApiUtil<>(updateArticle);
     }
 
 }
--- src/main/java/com/example/demo/_domain/blog/service/BlogService.java
+++ src/main/java/com/example/demo/_domain/blog/service/BlogService.java
@@ -40,4 +40,27 @@ public class BlogService {
         return postRepository.findById(id).orElseThrow(() -> new Exception400("해당 게시글이 없습니다."));
     }
 
+    // 수정 비즈니스 로직에 대한 생각!
+    // 영속성 컨텍스트에서 또는 DB에 존재하는 Article 엔티티(row)를 가지고 와서
+    // 상태 값을 수정하고 그 결과를 호출한 곳을 반환 한다.
+    @Transactional
+    public Article update(Integer id, ArticleDTO dto) {
+        // 수정 로직
+        Article articleEntity = postRepository
+                                .findById(id)
+                                .orElseThrow(() -> new Exception400("NOT FOUND : " + id));
+        // 객체 상태 값 변경
+        articleEntity.update(dto.getTitle(), dto.getContent());
+
+        // 영속성 컨텍스트 - 더티 체킹 알아보기
+        // 레포지토리의 save() 메서드는 수정할 때도 사용 가능하다.
+        // 단, 호출하지 않는 이유는 더티 체킹(Dirty Checking)이 동작하기 때문
+        // 즉, 트랜잭션 커밋 시 자동으로 영속성 컨텍스트와 데이터페이스(DB)에 변경 사항이 반영된다.
+
+        // DB에 save 처리
+        // postRepository.save(articleEntity);
+
+        return articleEntity;
+    }
+
 }
